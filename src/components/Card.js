1fbf9bf86500828165f5ff166f74669b883f5e9b
Nicolas Orjuela <n.orjuela@uniandes.edu.co>
2022-04-02 20:45:04 -0500

    a

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,15 +4,16 @@ import './Card.css';
 export const Card = (props) => {
   const { name, image, gender, status } = props;
   const estilo = {
-    width: '18rem'
+    width: '14rem'
   }
   return (
     <div className='card m-3' style={estilo}>
       <img className='card-img-top' src={image} alt={name} />
       <div className='card-body'>
         <h5 className='card-title'>{name}</h5>
-        <p>Genero: {gender}</p>
-        <p>Estado: {status}</p>
+        <p>Gender: {gender}</p>
+        <p>Status: {status}</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   );
